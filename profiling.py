a159f314ec53bec31edc556b87aa453daca479aa
ak19qp <78326809+ak19qp@users.noreply.github.com>
2022-09-24 14:58:07 -0400

    Add files via upload

---
+++ profiling.py
@@ -0,0 +1,84 @@
+import os
+import pandas
+from pandas import read_csv
+
+pandas.set_option('display.max_rows', None)
+
+os.system("perf stat -e 'syscalls:sys_enter_*' -a sleep 2 2>&1 | awk '$1 != 0' > perf.txt")
+
+
+
+
+dfsyscallmap = read_csv("syscallmap.csv")
+
+
+
+
+f = open("perf.txt", "r")
+
+
+temp = []
+new_temp = []
+
+for line in f:
+   temp.append(line)
+
+
+
+f.close()
+
+
+for n in temp[3:-3]:
+  sp = n.split()
+  new_temp.append(sp[0].replace(',', '')+","+sp[1][19:])
+
+
+
+
+f = open("new.txt", "w")
+
+f.write("Frequency,Syscall\n")
+for x in new_temp:
+  f.write(x+"\n")
+
+f.close()
+
+
+df = read_csv("new.txt")
+
+
+df = df.sort_values('Frequency', ascending=False)
+
+df = df.reset_index(drop=True)
+
+
+
+print("Syscalls sorted (desc) by freq.:")
+print("----------------------------------------------")
+print(df)
+print("----------------------------------------------\n")
+
+
+
+kernelprofiling = []
+
+
+for i in range(len(df.index)):
+  syscalled = df.loc[i]["Syscall"]
+  find = dfsyscallmap.loc[dfsyscallmap['Syscall'] == syscalled]
+  if find.empty == 0:
+    kernelprofiling.append(find.iloc[0,1])
+
+
+
+print("Kernel components accessed sorted by priority:")
+print("----------------------------------------------")
+for k in kernelprofiling:
+  print(k)
+
+
+
+
+
+
+
