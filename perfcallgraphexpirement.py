bfa93a50de11451ff93d6777d0ce2a5375bf63ab
ak19qp <78326809+ak19qp@users.noreply.github.com>
2022-10-12 00:58:48 -0400

    Update perfcallgraphexpirement.py

--- perfcallgraphexpirement.py
+++ perfcallgraphexpirement.py
@@ -28,90 +28,94 @@ class LinkedList:
         while (temp):
             lasttemp = temp
             temp = temp.next
-
         return lasttemp
 
+    def getData(self):
+        temp = self.head
+        return temp.data
+
+    def searchValue(self, val):
+        temp = self.head
+        while (temp):
+            if temp.data == val:
+                return True
+            temp = temp.next
+        return False
 
 
 
 
 
-# Code execution starts here
-if __name__ == '__main__':
-
-    # array=[]
-  
-    # # Start with the empty list
-    # llist = LinkedList()
-  
-    # llist.head = Node(1)
-    # second = Node(2)
-    # third = Node(3)
-  
-    # llist.head.next = second  # Link first node with second
-    # second.next = third  # Link second node with the third node
-  
-    # llist.printList()
 
 
-    #########################################
+array=[]
+last_parrent_index = 0
+with open('perf0.txt') as f:
+    while True:
 
-    array=[]
+        line = f.readline().strip()
+        if not line:
+            break
+        
 
-    with open('perf0.txt') as f:
-        while True:
-            line = f.readline().strip()
-            if not line:
-                break
+        if line != None:
+            if "[.]" in line:
+                parent = line.split("%  ")[2].split(" ",1)[0]
+                child = line.split("[.] ",1)[1]
 
-            #print(line)
+                #check if parent already exist
+                found = False
+                counter = -1
+                for k in array:
+                    counter = counter + 1
+                    if k.getData() == parent:
+                        k.head.headcounter = k.head.headcounter + 1
+                        last_parrent_index = counter
+                        found = True
+                        break
+                if not found:
+                    llist = LinkedList()
+                    llist.head = Node(parent)
+                    llist.head.next = Node(child)
+                    array.append(llist)
+                    last_parrent_index = len(array) - 1
 
+            elif "[k]" in line:
+                parent = line.split("%  ")[2].split(" ",1)[0]
+                child = line.split("[k] ",1)[1]
+
+                #check if parent already exist
+                found = False
+                counter = -1
+                for k in array:
+                    counter = counter + 1
+                    if k.getData() == parent:
+                        k.head.headcounter = k.head.headcounter + 1
+                        last_parrent_index = counter
+                        found = True
+                        break
+
+                if not found:
+                    llist = LinkedList()
+                    llist.head = Node(parent)
+                    llist.head.next = Node(child)
+                    array.append(llist)
+                    last_parrent_index = len(array) - 1
+
+            else:
+                children = line.split("% ",1)[1].split(";") 
+                temp = array[last_parrent_index].getLast()
+
+                for k in children:
+                    if array[-1].searchValue(k):
+                        continue
+                    child = Node(k)
+                    temp.next = child
+                    temp = child
+
+        
+           
             
-
-            if line != None:
-                if "[.]" in line:
-                    parent = line.split("%  ")[2].split(" ",1)[0]
-                    child = line.split("[.] ",1)[1]
-
-                    #check if parent already exist
-                    for k in array:
-                        if k.data = parent:
-                            k.headcounter = k.headcounter + 1
-                            break
-
-                    llist = LinkedList()
-                    llist.head = Node(parent)
-                    llist.head.next = Node(child)
-                    array.append(llist)
-
-                elif "[k]" in line:
-                    parent = line.split("%  ")[2].split(" ",1)[0]
-                    child = line.split("[k] ",1)[1]
-
-                     #check if parent already exist
-                    for k in array:
-                        if k.data = parent:
-                            k.headcounter = k.headcounter + 1
-                            break
-
-                    llist = LinkedList()
-                    llist.head = Node(parent)
-                    llist.head.next = Node(child)
-                    array.append(llist)
-                else:
-                    children = line.split("% ",1)[1].split(";") 
-                    temp = array[-1].getLast()
-                    for k in children:
-                        child = Node(k)
-                        temp.next = child
-                        temp = child
-               
-                
-    for k in array:
-
-
-
-                #head
-                #next line
-                #split ";"
-                #add them all into nodes and list
+for k in array:
+    k.printList()
+    print("\n")
