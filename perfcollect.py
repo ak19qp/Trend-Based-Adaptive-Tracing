095300213a8d3618cb1f5b3851822613dd547f6c
ak19qp <78326809+ak19qp@users.noreply.github.com>
2022-10-24 23:04:34 -0400

    Update perfcollect.py

--- perfcollect.py
+++ perfcollect.py
@@ -1,15 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import os
 
 
-sample_period = 1
-sample_collection = 5
+sample_period = 0.2 #total sampling in seconds per sample
+sample_collection = 100 #how many samples
+
+os.system('mkdir perfdata')
 
 for i in range(sample_collection):
-	os.system('sudo perf record -F 99 -a -g -o perf'+str(i)+'.data sleep '+str(sample_period))
-	os.system('sudo chmod 777 perf'+str(i)+'.data')
+	os.system('sudo perf record -F 999 -ag -o perfdata/perf'+str(i)+'.data sleep '+str(sample_period))
+	os.system('sudo chmod 777 perfdata/perf'+str(i)+'.data')
 
 
 
 for i in range(sample_collection):
-	os.system('sudo perf report -i perf'+str(i)+'.data --stdio -g folded -q > perf'+str(i)+'.txt')
-	os.system('sudo chmod 777 perf'+str(i)+'.txt')
+	os.system('sudo perf report -i perfdata/perf'+str(i)+'.data --stdio -q -g folded > perfdata/perf'+str(i)+'.txt')
+	os.system('sudo chmod 777 perfdata/perf'+str(i)+'.txt')
+	os.system('sudo rm perfdata/perf'+str(i)+'.data')
+	print(str(100*i/sample_collection)+"% done")
+
